6a395ad3849c4f6297c5242673f6ae3fac04e0ae
caiohman <cbalthazar70@gmail.com>
2024-10-15 18:27:46 -0300

    more queries

--- doc/queries.sql
+++ doc/queries.sql
@@ -18,3 +18,34 @@ select do2.day_off_date as day_off, do2.day_off_date - CURRENT_DATE as days_to_d
 from day_off do2
 join tech t on t.cpf = do2.cpf
 	where t.cpf = '';
+
+-- List atm's parts
+select pc.* 
+from parts_catalog pc
+join part_model pm on pm.part = pc.partnumber  
+join atm a on a.model = pm.model
+	where a.id = 50392;
+
+-- List atm's parts
+select pc.* 
+from parts_catalog pc
+join part_model pm on pm.part = pc.partnumber  
+join atm a on a.model = pm.model
+	where a.id = 50392 and pc.part_name like 'Leitor%';
+
+-- check day off of two employees
+with employee_one as 
+(
+	select do2.day_off_date as day_off, do2.day_off_date - CURRENT_DATE as days_to_day_off
+	from day_off do2
+	join tech t on t.cpf = do2.cpf
+		where t.cpf = ''
+),
+employee_two as
+(
+	select do2.day_off_date as day_off, do2.day_off_date - CURRENT_DATE as days_to_day_off
+	from day_off do2
+	join tech t on t.cpf = do2.cpf
+		where t.cpf = ''
+)	
+select day_off, days_to_day_off from employee_one union all select day_off, days_to_day_off from employee_two order by day_off asc; 
